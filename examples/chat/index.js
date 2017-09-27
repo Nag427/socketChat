e9f9715d67cf40159538373eb6270f0d6666868c
nagdasari <nagdsr@gmail.com>
2017-09-26 23:47:03 -0400

    socket with basic rooms

---
+++ examples/chat/index.js
@@ -0,0 +1,111 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('../..')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+var numUsers = 0;
+var users=[];
+var rooms=[];
+function checkRoom(room){
+var index= rooms.indexOf(room);
+console.log(index);
+if(index>-1)
+	return true;
+else return false;
+}
+
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  io.emit('room added', {
+	  username: socket.username,
+	  rooms:rooms
+	  });// when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+  
+  socket.on('add room',function(room){
+	  if(checkRoom(room))return;
+	  rooms.push(room);
+	  //console.log("room added would be broadcasted"+rooms[0]);
+	  
+	  io.emit('room added', {
+	  username: socket.username,
+	  rooms:rooms
+	  });
+	  /*socket.emit('login', {
+      numUsers: numUsers
+    });
+	 */
+	  
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+	users.push(username);
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+	console.log(users);
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+		var index = users.indexOf(socket.username);
+		if (index > -1) {
+    users.splice(index, 1);
+}
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+	  console.log(users);
+    }
+  });
+});
